c88edff450c7de3fd665158626c519224e21ceff
Alex Kost <alezost@gmail.com>
2018-09-09 21:45:01 +0300

    Update version to 0.2.1
    This should have been done before "v0.2.1" tag was made on the previous
    commit, but well...

--- build-farm.el
+++ build-farm.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2015–2018 Alex Kost <alezost@gmail.com>
 
 ;; Author: Alex Kost <alezost@gmail.com>
-;; Version: 0.2
+;; Version: 0.2.1
 ;; URL: https://gitlab.com/alezost-emacs/build-farm
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "24.4") (bui "1.1.0") (magit-popup "2.1.0"))
